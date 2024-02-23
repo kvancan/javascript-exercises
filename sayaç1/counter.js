0c884e46ab4995ca2e1e66d1af05344870ef6145
Kıvanç CAN <129111868+kvancan@users.noreply.github.com>
2024-02-23 22:37:09 +0300

    Add files via upload

---
+++ sayaç1/counter.js
@@ -0,0 +1,24 @@
+
+function kilicdaraoyver(){
+
+    toplam = 0;
+
+    toplam = 1 + toplam;
+
+    document.getElementById("kilicdar").value = toplam;
+    
+
+
+}
+
+function tayyibeoyver(){
+
+    sonuç = 0;
+
+    sonuç = 1 + sonuç;
+
+    document.getElementById("tayyip").value = sonuç;
+    
+
+
+}
