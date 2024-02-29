5e29c0af593f7bbb2a029b729b44c127f082590e
Kıvanç CAN <129111868+kvancan@users.noreply.github.com>
2024-03-01 00:47:54 +0300

    Add files via upload

---
+++ vote/vote.js
@@ -0,0 +1,12 @@
+var oylar = 0;
+var oylar1 = 0;
+
+function onClick() {
+  oylar += 1;
+  document.getElementById("oylar").innerHTML = oylar;
+};
+
+function onClick1() {
+    oylar1 += 1;
+    document.getElementById("oylar1").innerHTML = oylar1;
+  };
