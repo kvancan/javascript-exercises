8fe6336811f70ff38f148f3eb3db97070cfecedc
Kıvanç CAN <129111868+kvancan@users.noreply.github.com>
2024-03-07 02:52:50 +0300

    Add files via upload

---
+++ vote/vote and calculate/vote.js
@@ -0,0 +1,58 @@
+var oylar = 0;
+var oylar1 = 0;
+var oylar2 = 0;
+
+function onClick() {
+  oylar += 1;
+  document.getElementById("oylar").innerHTML = oylar;
+  percent();
+  percent1();
+  percent2();
+};
+
+function onClick1() {
+    oylar1 += 1;
+    document.getElementById("oylar1").innerHTML = oylar1;
+    percent();
+    percent1();
+    percent2();
+  };
+
+  function onClick2() {
+    oylar2 += 1;
+    document.getElementById("oylar2").innerHTML = oylar2;
+    percent();
+    percent1();
+    percent2();
+  };
+
+function percent() {
+
+  toplam = oylar+oylar1+oylar2; 
+  oylaryuzde = oylar/toplam;
+  oylaryuzde = oylaryuzde*100;
+  oylaryuzde = oylaryuzde.toFixed(1);
+  document.getElementById("percent").innerHTML = oylaryuzde;
+
+}
+function percent1() {
+
+  toplam = oylar+oylar1+oylar2; 
+  oylaryuzde1 = oylar1/toplam;
+  oylaryuzde1 = oylaryuzde1*100;
+  oylaryuzde1 = oylaryuzde1.toFixed(1);
+
+  document.getElementById("percent1").innerHTML = oylaryuzde1;
+
+}
+function percent2() {
+
+  toplam = oylar+oylar1+oylar2; 
+  oylaryuzde2 = oylar2/toplam;
+  oylaryuzde2 = oylaryuzde2*100;
+  oylaryuzde2 = oylaryuzde2.toFixed(1);
+  
+  document.getElementById("percent2").innerHTML = oylaryuzde2;
+
+}
+
